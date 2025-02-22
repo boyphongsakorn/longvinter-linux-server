de784606596893b4f11098910092c660db2ca371
Phongsakorn Wisetthon <boy1556@hotmail.com>
2025-02-22 07:02:04 +0700

    Add design file

--- LongvinterUpdate.sh
+++ LongvinterUpdate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo checking for updates
+echo "Checking for updates..."
 
 UpdateServer () {
         systemctl is-active --quiet longvinter && echo "Server is running!"
-        sudo systemctl stop longvinter && echo "Server is now closed, now getting latest version..."
+        sudo systemctl stop longvinter && echo "Server is now closed, now getting latest version.."
         git restore .
         sleep 1
-        git pull "https://github.com/Uuvana-Studios/longvinter-linux-server.git" official
+        git stash
+        git pull "https://github.com/Uuvana-Studios/longvinter-linux-server.git" main
         sleep 1
         echo "Starting server..."
         sudo chmod -R ugo+rwx /home/nilsson/longvinter-linux-server/
