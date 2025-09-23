36bdcb9aaa2fa17c76a56c6a88f3fd0386067174
Phongsakorn Wisetthon <3013842+boyphongsakorn@users.noreply.github.com>
2025-09-23 21:50:34 +0700

    Update LongvinterUpdate.sh

--- LongvinterUpdate.sh
+++ LongvinterUpdate.sh
@@ -2,8 +2,9 @@
 echo "Checking for updates..."
 
 UpdateServer () {
-        systemctl is-active --quiet longvinter && echo "Server is running!"
-        sudo systemctl stop longvinter && echo "Server is now closed, now getting latest version.."
+        # systemctl is-active --quiet longvinter && echo "Server is running!"
+        # sudo systemctl stop longvinter && echo "Server is now closed, now getting latest version.."
+        echo "getting latest version.."
         git restore .
         sleep 1
         git stash
@@ -11,7 +12,8 @@ UpdateServer () {
         sleep 1
         echo "Starting server..."
         sudo chmod -R ugo+rwx /data/longvinter-linux-server/
-        sudo systemctl start longvinter && echo "Server is now running"
+        # sudo systemctl start longvinter && echo "Server is now running"
+        echo "Server is now comeing soon to running"
 }
 
 cd /data/longvinter-linux-server
